b304383a4963d06617e0845cf5863f869d179008
retOK1986 <retOK@i.ua>
2024-02-23 00:17:36 +0200

    add models and alembic

---
+++ DZ_7/app/models/models.py
@@ -0,0 +1,41 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Date, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class Student(Base):
+    __tablename__ = 'students'
+    id = Column(Integer, primary_key=True)
+    fullname = Column(String)
+    group_id = Column(Integer, ForeignKey('groups.id'))
+    group = relationship("Group", back_populates="students")
+
+class Group(Base):
+    __tablename__ = 'groups'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    students = relationship("Student", back_populates="group")
+
+class Teacher(Base):
+    __tablename__ = 'teachers'
+    id = Column(Integer, primary_key=True)
+    fullname = Column(String)
+    subjects = relationship("Subject", back_populates="teacher")
+
+class Subject(Base):
+    __tablename__ = 'subjects'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    teacher_id = Column(Integer, ForeignKey('teachers.id'))
+    teacher = relationship("Teacher", back_populates="subjects")
+
+class Grade(Base):
+    __tablename__ = 'grades'
+    id = Column(Integer, primary_key=True)
+    student_id = Column(Integer, ForeignKey('students.id'))
+    subject_id = Column(Integer, ForeignKey('subjects.id'))
+    grade = Column(Float)
+    date_received = Column(Date)
+    student = relationship("Student")
+    subject = relationship("Subject")
