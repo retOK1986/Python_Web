8c3bdf75b9c9bf8b42e3aa4ce266d04b4e626175
retOK1986 <retOK@i.ua>
2024-03-02 23:39:19 +0200

    change seed.py

--- DZ_7/app/data/seed.py
+++ DZ_7/app/data/seed.py
@@ -13,42 +13,40 @@ faker = Faker('uk-UA')
 
 # Створення груп
 for _ in range(3):
-    group = Group(name=faker.word())
+    group = Group(name=f'{faker.word()} {faker.random_digit()}')
     session.add(group)
 session.commit()
 
 # Створення викладачів
-teachers = []
 for _ in range(3):
     teacher = Teacher(fullname=faker.name())
     session.add(teacher)
-    teachers.append(teacher)
 session.commit()
 
 # Створення предметів
-subjects = []
+teachers = session.query(Teacher).all()
 for _ in range(5):
-    subject = Subject(name=faker.word(), teacher=random.choice(teachers))
+    subject = Subject(name=f'{faker.word()} {faker.random_digit()}', teacher=random.choice(teachers))
     session.add(subject)
-    subjects.append(subject)
 session.commit()
 
 # Створення студентів і оцінок
 groups = session.query(Group).all()
-for _ in range(50):
+subjects = session.query(Subject).all()
+for _ in range(50):  # Створюємо близько 50 студентів
     student = Student(fullname=faker.name(), group=random.choice(groups))
     session.add(student)
     session.commit()  # Зберігаємо студента, щоб отримати ID для зв'язку з оцінками
 
     for subject in subjects:
-        for _ in range(random.randint(5, 20)):  # Кількість оцінок для кожного студента по предмету
+        for _ in range(random.randint(1, 20)):  # До 20 оцінок для кожного студента по кожному предмету
             grade = Grade(
-                student_id=student.id,
-                subject_id=subject.id,
                 grade=random.randint(1, 12),  # Припустимо, що оцінки від 1 до 12
-                date_received=faker.date_between(start_date='-2y', end_date='today')
+                grade_date=faker.date_between(start_date='-2y', end_date='today'),
+                student_id=student.id,
+                subjects_id=subject.id
             )
             session.add(grade)
-session.commit()
+    session.commit()
 
 print("База даних успішно заповнена випадковими даними.")
